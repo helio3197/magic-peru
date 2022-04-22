31db1058bd41b22fd33fc91722572b712a335520
Kenny <kennysalazar3197@gmail.com>
2022-04-22 11:42:22 -0500

    Fix bug on the animateNumbers component

--- src/components/Homepage/utils/AnimateNumber.js
+++ src/components/Homepage/utils/AnimateNumber.js
@@ -38,6 +38,8 @@ const AnimateNumber = ({
     } else {
       window.addEventListener('scroll', runAnimation);
     }
+
+    return () => window.removeEventListener('scroll', runAnimation);
   }, []);
 
   return (
