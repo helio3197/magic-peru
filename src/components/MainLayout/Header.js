b492bed8013005414bddf11c87977b9c4ef9716c
Kenny <kennysalazar3197@gmail.com>
2022-05-20 01:17:01 -0500

    Fix bug in the Header component

--- src/components/MainLayout/Header.js
+++ src/components/MainLayout/Header.js
@@ -16,13 +16,25 @@ const Header = () => (
       <Navbar.Toggle aria-controls="navbar-nav" />
       <Navbar.Collapse id="navbar-nav">
         <Nav className="mx-auto gap-sm-3">
-          <Nav.Link as={toNavLink} href="/" style={({ isActive }) => (isActive ? { color: 'white' } : {})}>
+          <Nav.Link
+            as={toNavLink}
+            href="/"
+            style={({ isActive }) => (isActive ? { color: 'white', textDecoration: 'underline' } : { color: '#fffefeb3' })}
+          >
             Inicio
           </Nav.Link>
-          <Nav.Link as={toNavLink} href="/nosotros" style={({ isActive }) => (isActive ? { color: 'white' } : {})}>
+          <Nav.Link
+            as={toNavLink}
+            href="/nosotros"
+            style={({ isActive }) => (isActive ? { color: 'white', textDecoration: 'underline' } : { color: '#fffefeb3' })}
+          >
             Quienes Somos
           </Nav.Link>
-          <Nav.Link as={toNavLink} href="/tours" style={({ isActive }) => (isActive ? { color: 'white' } : {})}>
+          <Nav.Link
+            as={toNavLink}
+            href="/tours"
+            style={({ isActive }) => (isActive ? { color: 'white', textDecoration: 'underline' } : { color: '#fffefeb3' })}
+          >
             Tours
           </Nav.Link>
         </Nav>
