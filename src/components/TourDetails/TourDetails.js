fd120b7ccadc3916d43aed35cc822cb9936a7469
Kenny <kennysalazar3197@gmail.com>
2022-05-20 02:02:07 -0500

    Initialize TourDetails component

--- src/components/TourDetails/TourDetails.js
+++ src/components/TourDetails/TourDetails.js
@@ -1,7 +1,12 @@
 import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import Contact from '../Homepage/Contact';
 import data from '../../data/info';
+import { toLink } from '../../utils';
 
 const TourDetails = () => {
   const { tourDetails } = useParams();
@@ -15,7 +20,31 @@ const TourDetails = () => {
 
   return (
     <Container fluid as="main" className="p-0 d-inline-block">
-      <h1 className="text-center mt-5 mb-4">{tourData.tourName}</h1>
+      <Container as="section">
+        <Breadcrumb className="mt-1 mt-sm-3">
+          <Breadcrumb.Item linkAs={toLink} href="/tours">
+            Volver a lista de tours
+          </Breadcrumb.Item>
+        </Breadcrumb>
+        <h1 className="text-center mb-4">{tourData.tourName}</h1>
+        <Row className="mt-1">
+          <Col xs="12" lg="8">
+            <Image src={tourData.tourImg[0]} fluid className="w-100" style={{ aspectRatio: '3 / 2' }} />
+          </Col>
+          <Col xs="12" lg="4" className="pt-3 pt-lg-0">
+            <h2>Descripción:</h2>
+            <p>
+              {tourData.description}
+            </p>
+          </Col>
+        </Row>
+        <h2 className="fs-3">Salidas:</h2>
+        <p>{tourData.departures}</p>
+        <h2 className="fs-3">Duración:</h2>
+        <p>{tourData.duration}</p>
+        <h2 className="fs-3">Idiomas:</h2>
+        <p>{tourData.idiomas}</p>
+      </Container>
       <Contact />
     </Container>
   );
